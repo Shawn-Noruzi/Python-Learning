5f8c5fd1542d8a5bc087fb698f466c0381d79723
shawnSFU <snoruzi@sfu.ca>
2018-09-01 19:03:03 -0700

    Basic PyMongo complete

--- Learning/PyMongo/Pymongo.py
+++ Learning/PyMongo/Pymongo.py
@@ -6,9 +6,18 @@ db = myClient.mydb
 
 #mongo SQL table is called a collection
 
+#create table
 users = db.users
 
-user1 = {"username": "nick", "pass": "123", "fav_num": "2", "hobbies":["games","python","moregames"]}
+#data to be put in
+user1 = {"username": "josh", "pass": "123", "fav_num": "2", "hobbies":["games","python","moregames"]}
 
+#how to put it in
 user_id = users.insert_one(user1).inserted_id
 
+
+#multi objects to insert
+user_many = [{"username": "third", "password":"123"}, {"username2": "third", "password":"123"},{"username": "third", "password":"123"}]
+
+#command is db.insert_many(obj)
+users.insert_many(user_many)
